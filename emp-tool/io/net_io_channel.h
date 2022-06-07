f378b3b70664b1c156a044e304847308e904644c
Mingxuan Fan <1417388068@qq.com>
2022-06-07 17:17:14 +0800

    Add rsocket APIs

--- emp-tool/io/net_io_channel.h
+++ emp-tool/io/net_io_channel.h
@@ -9,6 +9,8 @@
 #include "emp-tool/io/io_channel.h"
 using std::string;
 
+#include <rdma/rsocket.h>
+#include <rdma/rdma_cma.h>
 
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -19,6 +21,8 @@ using std::string;
 
 namespace emp {
 
+uint64_t wr_id = 0;
+
 class NetIO: public IOChannel<NetIO> { public:
 	bool is_server;
 	int mysocket = -1;
@@ -28,12 +32,22 @@ class NetIO: public IOChannel<NetIO> { public:
 	bool has_sent = false;
 	string addr;
 	int port;
-	NetIO(const char * address, int port, bool quiet = false) {
-		if (port <0 || port > 65535) {
+	bool rs;
+	NetIO(const char * address, int port, bool quiet = false, bool rs = false) {
+		if (port < 0 || port > 65535) {
 			throw std::runtime_error("Invalid port number!");
 		}
 
+		if (rs) {
+			printf("Using RDMA Socket\n");
+		}
+		else {
+			printf("Using TCP Socket\n");
+		}
+
 		this->port = port;
+		this->rs = rs;
+
 		is_server = (address == nullptr);
 		if (address == nullptr) {
 			struct sockaddr_in dest;
@@ -43,19 +57,38 @@ class NetIO: public IOChannel<NetIO> { public:
 			serv.sin_family = AF_INET;
 			serv.sin_addr.s_addr = htonl(INADDR_ANY); /* set our address to any interface */
 			serv.sin_port = htons(port);           /* set the server port number */
-			mysocket = socket(AF_INET, SOCK_STREAM, 0);
+			
 			int reuse = 1;
-			setsockopt(mysocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse));
-			if(bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr)) < 0) {
-				perror("error: bind");
-				exit(1);
+			if (rs) {
+				mysocket = rsocket(AF_INET, SOCK_STREAM, 0);
+				rsetsockopt(mysocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse));
+				
+				if(rbind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr)) < 0) {
+					perror("error: bind");
+					exit(1);
+				}
+				if(rlisten(mysocket, 1) < 0) {
+					perror("error: listen");
+					exit(1);
+				}
+				consocket = raccept(mysocket, (struct sockaddr *)&dest, &socksize);
+				rclose(mysocket);
 			}
-			if(listen(mysocket, 1) < 0) {
-				perror("error: listen");
-				exit(1);
+			else {
+				mysocket = socket(AF_INET, SOCK_STREAM, 0);
+				setsockopt(mysocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse));
+				
+				if(bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr)) < 0) {
+					perror("error: bind");
+					exit(1);
+				}
+				if(listen(mysocket, 1) < 0) {
+					perror("error: listen");
+					exit(1);
+				}
+				consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+				close(mysocket);
 			}
-			consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
-			close(mysocket);
 		}
 		else {
 			addr = string(address);
@@ -65,15 +98,21 @@ class NetIO: public IOChannel<NetIO> { public:
 			dest.sin_family = AF_INET;
 			dest.sin_addr.s_addr = inet_addr(address);
 			dest.sin_port = htons(port);
-
 			while(1) {
-				consocket = socket(AF_INET, SOCK_STREAM, 0);
-
-				if (connect(consocket, (struct sockaddr *)&dest, sizeof(struct sockaddr)) == 0) {
-					break;
+				if (rs) {
+					consocket = rsocket(AF_INET, SOCK_STREAM, 0);
+					if (rconnect(consocket, (struct sockaddr *)&dest, sizeof(struct sockaddr)) == 0) {
+						break;
+					}
+					rclose(consocket);
 				}
-
-				close(consocket);
+				else {
+					consocket = socket(AF_INET, SOCK_STREAM, 0);
+					if (connect(consocket, (struct sockaddr *)&dest, sizeof(struct sockaddr)) == 0) {
+						break;
+					}
+					close(consocket);
+				}
 				usleep(1000);
 			}
 		}
@@ -105,25 +144,42 @@ class NetIO: public IOChannel<NetIO> { public:
 	}
 
 	void set_nodelay() {
-		const int one=1;
-		setsockopt(consocket,IPPROTO_TCP,TCP_NODELAY,&one,sizeof(one));
+		const int one = 1;
+		if (this->rs) {
+			rsetsockopt(consocket,IPPROTO_TCP,TCP_NODELAY,&one,sizeof(one));
+		}
+		else {
+			setsockopt(consocket,IPPROTO_TCP,TCP_NODELAY,&one,sizeof(one));
+		}
 	}
 
 	void set_delay() {
 		const int zero = 0;
-		setsockopt(consocket,IPPROTO_TCP,TCP_NODELAY,&zero,sizeof(zero));
+		if (this->rs) {
+			rsetsockopt(consocket,IPPROTO_TCP,TCP_NODELAY,&zero,sizeof(zero));
+		}
+		else {
+			setsockopt(consocket,IPPROTO_TCP,TCP_NODELAY,&zero,sizeof(zero));
+		}
 	}
 
 	void flush() {
 		fflush(stream);
 	}
 
+	// void printData(void* data, size_t len) {
+	// 	for (size_t i = 0; i < len; i++) {
+	// 		printf("0x%x ", *(i + (char*)data));
+	// 	}
+	// 	printf("\n");
+	// }
+
 	void send_data_internal(const void * data, size_t len) {
 		size_t sent = 0;
 		while(sent < len) {
-			size_t res = fwrite(sent + (char*)data, 1, len - sent, stream);
+			size_t res = this->rs ? rwrite(consocket, sent + (char*)data,  len - sent) : fwrite(sent + (char*)data, 1, len - sent, stream);
 			if (res > 0)
-				sent+=res;
+				sent += res;
 			else
 				error("net_send_data\n");
 		}
@@ -131,12 +187,13 @@ class NetIO: public IOChannel<NetIO> { public:
 	}
 
 	void recv_data_internal(void  * data, size_t len) {
-		if(has_sent)
+		if(has_sent) {
 			fflush(stream);
+		}
 		has_sent = false;
 		size_t sent = 0;
 		while(sent < len) {
-			size_t res = fread(sent + (char*)data, 1, len - sent, stream);
+			size_t res = this->rs ? rread(consocket, sent + (char*)data,  len - sent) : fread(sent + (char*)data, 1, len - sent, stream);
 			if (res > 0)
 				sent += res;
 			else
